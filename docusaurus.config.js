b8c8f5c852dd7412a12f728c76370b57d9f66a4c
CubeBeveled <cubebeveled@gmail.com>
2024-04-02 19:42:53 +0200

    Changed social card to logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo.jpg',
       navbar: {
         title: 'The 6b6t wiki',
         logo: {

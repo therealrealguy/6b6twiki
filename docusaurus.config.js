89d943188bc09c38f149d17296c6f9406492b3f1
advik555 <141858080+therealrealguy@users.noreply.github.com>
2024-04-22 20:49:33 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/icons/favicon.ico',
   onBrokenLinks: 'warn'
   // Set the production url of your site here
-  url: 'https://6b6t-wiki.vercel.app/',
+  url: 'https://6b6t-wiki.vercel.app/'
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
